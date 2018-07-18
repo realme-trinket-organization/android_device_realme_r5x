03b62ba40baee314a43a5e205c865ca2468b2edd
Danny Baumann <dannybaumann@web.de>
2018-07-18 17:25:32 +0200

    RMX1911: Enable call recording
    Change-Id: I1b4478b330b13cf5d0fe1ed0d40f43e4b3825f33

--- device.mk
+++ device.mk
@@ -19,7 +19,8 @@ $(call inherit-product, vendor/realme/RMX1911/RMX1911-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
